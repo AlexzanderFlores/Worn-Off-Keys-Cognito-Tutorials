04f4c8248257cfe4dcac7bbc182f6a3042f1ffeb
AlexzanderFlores <alexzanderflores@nestnetworking.com>
2020-01-06 09:33:00 -0800

    Add video 4: Cognito change password

---
+++ 4-Cognito-Change-Password/src/components/Settings.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, useContext } from "react";
+import { AccountContext } from "./Accounts";
+import ChangePassword from "./ChangePassword";
+
+export default () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const { getSession } = useContext(AccountContext);
+
+  useEffect(() => {
+    getSession().then(() => {
+      setLoggedIn(true);
+    });
+  }, []);
+
+  return (
+    <div>
+      {loggedIn && (
+        <>
+          <h1>Settings</h1>
+
+          <ChangePassword />
+        </>
+      )}
+    </div>
+  );
+};
